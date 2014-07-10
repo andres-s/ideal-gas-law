df4032cab2dad449c943e78601c60966625eee4f
andres-s <a.sarnari@gmail.com>
2014-07-10 23:50:04 +1000

    One molecule now created at random position

--- gases.js
+++ gases.js
@@ -1,15 +1,51 @@
 
-(function() {
+var GasBox = (function() {
+
     var svgNS = "http://www.w3.org/2000/svg";
 
-    window.setTimeout(function() {
-        var container = document.getElementById('container');    
+    function GasBox(htmlElem, numMolecules, moleculeRadius) {
+        this._htmlElem = htmlElem;
+        this._initialise(numMolecules, moleculeRadius);
+        return this;
+    }
+
+    GasBox.prototype.height = function() {
+        return this._htmlElem.clientHeight;
+    };
+
+    GasBox.prototype.width = function() {
+        return this._htmlElem.clientWidth;
+    };
+
+    GasBox.prototype._initialise = function(numMolecules, moleculeRadius) {
+
+        var heightRange = this.height() - 2*moleculeRadius;
+        var widthRange  = this.width() - 2*moleculeRadius;
+        
+        for (var i = 0; i < numMolecules; i++) {
+            var x = Math.random() * widthRange + moleculeRadius;
+            var y = Math.random() * heightRange + moleculeRadius;
+            this._placeCircle(x, y, moleculeRadius);
+        }
+    };
+
+    GasBox.prototype._placeCircle = function(x, y, r) {
+        r = r || 10;
         var myCircle = document.createElementNS(svgNS, 'circle');
-        myCircle.setAttributeNS(null,"cx",100);
-        myCircle.setAttributeNS(null,"cy",100);
-        myCircle.setAttributeNS(null,"r",50);
-        myCircle.setAttributeNS(null,"fill","black");
-        myCircle.setAttributeNS(null,"stroke","none");
-        container.appendChild(myCircle);
-    }, 1500);
+        myCircle.setAttributeNS(null, "cx", x);
+        myCircle.setAttributeNS(null, "cy", y);
+        myCircle.setAttributeNS(null, "r", r);
+        myCircle.setAttributeNS(null, "fill","black");
+        myCircle.setAttributeNS(null, "stroke","none");
+        this._htmlElem.appendChild(myCircle);
+    };
+
+    return GasBox;
+
+})();
+
+(function() {
+
+    var gasBox = new GasBox(document.getElementById('box'), 1, 10);
+
 })();
