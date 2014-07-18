07e8bdc28d4c0ab1adeda65bf8eac1348e0c4205
andres-s <a.sarnari@gmail.com>
2014-07-18 18:15:11 +1000

    Added step button

--- ui.js
+++ ui.js
@@ -4,14 +4,20 @@ var userinterface =  (function () {
     var Animator = animator.Animator;
 
     var START_CLASS = 'start-button',
-        STOP_CLASS  = 'stop-button';
+        STOP_CLASS  = 'stop-button',
+        PRESS_CLASS = 'pressable',
+        DELTA_MILLIS = 13;
 
 
     function UserInterface(animation) {
         this._animation = new Animator(document.getElementById('box'));
-        this._stopstart = new StopStartButton();
         this._runningRealTime = true;
-        this._stopstart
+
+        this._stepButton = new StepButton(this.step.bind(this));
+        this._stepButton.disable();
+
+        this._stopstartButton = new StopStartButton();
+        this._stopstartButton
             .addEventListener('click', 
                               this.toggleRealTime.bind(this));
     }
@@ -19,13 +25,20 @@ var userinterface =  (function () {
     UserInterface.prototype.toggleRealTime = function() {
         if (this._runningRealTime) {
             this._animation.stop();
-            this._stopstart.displayStart();
+            this._stopstartButton.displayStart();
+            this._stepButton.enable();
         } else {
             this._animation.realTimeAnimate();
-            this._stopstart.displayStop();
+            this._stepButton.disable();
+            this._stopstartButton.displayStop();
         }
         this._runningRealTime = !this._runningRealTime;
     };
+
+    UserInterface.prototype.step = function() {
+        this._animation.manualAdvance(DELTA_MILLIS);
+    };
+
 
     function StopStartButton () {
         this._labelElem = document.getElementById('stop-start-button');
@@ -44,8 +57,25 @@ var userinterface =  (function () {
     };
 
     StopStartButton.prototype.addEventListener = function() {
-        this._labelElem.addEventListener.apply(this._buttonElem, arguments);
+        this._labelElem.addEventListener.apply(this._labelElem, arguments);
     };
+
+
+    function StepButton (stepFunction) {
+        this._stepFunction = stepFunction;
+        this._buttonElem = document.getElementById('step-button');
+    }
+
+    StepButton.prototype.disable = function() {
+        this._buttonElem.removeEventListener('click', this._stepFunction);
+        this._buttonElem.classList.remove(PRESS_CLASS);
+    };
+
+    StepButton.prototype.enable = function() {
+        this._buttonElem.classList.add(PRESS_CLASS);
+        this._buttonElem.addEventListener('click', this._stepFunction);
+    };
+
 
     return {
         UserInterface: UserInterface
